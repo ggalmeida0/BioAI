5e6afa4c86ce4ca2a4563aec6c89ad590fff12ca
Gabriel <ggalmeida0@gmail.com>
2023-07-01 11:28:08 -0500

    Adding logging when resolving expo constants for debugging deployments

--- ui/app.config.js
+++ ui/app.config.js
@@ -1,39 +1,46 @@
 const env = process.env.EXPO_ENV;
 
-const apiEndpoint = env === 'prod' ? 'https://uudihltgbu4wqxm3nckfe7ylgi0kzccx.lambda-url.us-east-2.on.aws' : 'http://localhost:3000';
-const authRedirectUri = env === 'prod' ? 'https://master.d3mqj86iwprlmp.amplifyapp.com/' : 'http://localhost:19006/'
+const apiEndpoint =
+  env === 'prod'
+    ? 'https://uudihltgbu4wqxm3nckfe7ylgi0kzccx.lambda-url.us-east-2.on.aws'
+    : 'http://localhost:3000';
+const authRedirectUri =
+  env === 'prod'
+    ? 'https://master.d3mqj86iwprlmp.amplifyapp.com/'
+    : 'http://localhost:19006/';
+
+console.log('API Endpoint: ', apiEndpoint);
+console.log('Auth Redirect URI', authRedirectUri);
 
 export default {
-  "expo": {
-    "name": "bio",
-    "slug": "bio",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: 'bio',
+    slug: 'bio',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      }
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: './assets/favicon.png',
     },
     extra: {
       apiEndpoint,
-      authRedirectUri
-    }
-  }
-}
+      authRedirectUri,
+    },
+  },
+};
